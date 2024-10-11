5e4fcef803c6b4449d2452b3946936f0f6f810fc
Trishan Wagle <mailtotrishan@gmail.com>
2024-10-11 18:23:59 +0545

    feat: tailwind setup, auth routes fix, login frontend

--- app/routes/templates_routes.py
+++ app/routes/templates_routes.py
@@ -1,21 +1,24 @@
 from flask import Blueprint, redirect, render_template, session, url_for
 from app.utils import login_required
 
-templates_bp = Blueprint('templates_bp', __name__)
+templates_bp = Blueprint("templates_bp", __name__)
 
-@templates_bp.route('/', methods=['GET'])
+
+@templates_bp.route("/", methods=["GET"])
 @login_required
 def home_page():
-    return render_template('index.html', user_name = session["user_name"])
+    return render_template("index.html", user_name=session["user_name"])
 
-@templates_bp.route('/signup', methods=['GET'])
+
+@templates_bp.route("/signup", methods=["GET"])
 def signup_page():
-    if session["user_id"]:
-            return redirect(url_for('templates_bp.home_page'))
-    return render_template('signup.html')
+    if session.get("user_id"):
+        return redirect(url_for("templates_bp.home_page"))
+    return render_template("signup.html")
 
-@templates_bp.route('/login', methods=['GET'])
+
+@templates_bp.route("/login", methods=["GET"])
 def login_page():
-    if session["user_id"]:
-        return redirect(url_for('templates_bp.home_page'))
-    return render_template('login.html')
+    if session.get("user_id"):
+        return redirect(url_for("templates_bp.home_page"))
+    return render_template("login.html")
